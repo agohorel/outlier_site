593b0aafc27c23f93519ee269e8a240580dd49f0
agohorel <alex.oddlogic@gmail.com>
2017-06-16 23:50:46 -0400

    logo mouseover animation
    -added animation on mouseover and mouseout
    -added main.js on all existing pages

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var logo = document.querySelector("#header-logo");
+var navbar = document.querySelector(".navbar-default"); 
+var logoText = document.querySelector(".navbar-brand");
 
 logo.addEventListener("mouseover", function(){
-	document.querySelector(".navbar-default").style.height = "100px";
+	navbar.style.height = "100px";
+	logoText.style.fontSize = "0px";
+	logoText.style.color = "#d3d3d3";
+
 });
 
 logo.addEventListener("mouseout", function(){
-	document.querySelector(".navbar-default").style.height = "50px";
+	navbar.style.height = "50px";
+	logoText.style.fontSize = "18px";
+	logoText.style.color = "#777777";
 });
 
