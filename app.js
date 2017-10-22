36436e03c92130676ba521c94af9b17a8967ce6b
agohorel <alex.oddlogic@gmail.com>
2017-10-22 17:46:32 -0400

    working homepage & new directories
    -created basic app.js
    -add img & scripts folders to public
    -relocate assets accordingly
    -converted home.html to home.ejs

---
+++ app.js
@@ -0,0 +1,16 @@
+var express      = require("express"),
+    app          = express(),
+    bodyParser   = require("body-parser");
+
+// mongoose.connect("mongodb://localhost/outlier");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+	res.render("../views/static/home.ejs");
+});
+
+ app.listen(8000, "127.0.0.1", function(){
+ 	console.log("outlier server is listening");
+ });
