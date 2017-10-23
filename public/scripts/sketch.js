3eb90cb1c01f2b8b7b1be8462932eb3efe813af7
agohorel <alex.oddlogic@gmail.com>
2017-10-22 23:19:31 -0400

    edit sketch.js

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -33,8 +33,8 @@ function draw() {
   translate(width / 2, height / 2);
   rotate(radians(45));
 
-  dampX = map(mouseX, 0, width, .1, .5);
-  dampY = map(mouseY, 0, height, .1, .5);
+  dampX = map(mouseX, 0, width, .25, .5);
+  dampY = map(mouseY, 0, height, .25, .5);
   dampen = dampX + dampY / 2;
   
     for (var i = 0; i < numLines; i++) {
@@ -48,11 +48,11 @@ function draw() {
   }
 
   if(second() % 60 <= 29){
-    t += 1 * dampen;
+    t += .5 * dampen;
     print("condition 1:", second(), second() % 60);
   }  else
   if(second() % 60 > 30){
-    t -= 1 * dampen;
+    t -= .5 * dampen;
     print("condition 2:", second(), second() % 60);
   }
   
